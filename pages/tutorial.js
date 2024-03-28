6e168483d5ad3db28b79c9c4ff716149e64acc66
ibertagnolli <ibertagnolli@hotmail.com>
2024-03-28 15:47:40 -0600

    basic tutorial page, need video

--- pages/tutorial.js
+++ pages/tutorial.js
@@ -1,7 +1,8 @@
 import { useRouter } from "next/router";
 import Navbar from "../components/Navbar";
-import styles from "../styles/index.module.css";
- // Import your Navbar component
+import Button from "@mui/material/Button";
+import theme from "../styles/theme";
+// Import your Navbar component
 
 const TeamMemberPage = () => {
     const router = useRouter();
@@ -9,26 +10,41 @@ const TeamMemberPage = () => {
         <>
             <Navbar
                 links={[
-                    { text: "Home", path:"/" },
+                    { text: "Home", path: "/" },
                     { text: "Team", path: "/team" },
                     { text: "Tutorial", path: "/tutorial" },
                 ]}
                 bgcolor="transparent"
             />
-            <div style={{ width: "100vw", minHeight: "100vh", display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "center", padding: "20px" }}>
-                <h2 style={{ fontSize: 50, color: "#0d4b5f", marginBottom: 10, marginTop: 30 }}><a href=""> DOWNLOAD NOW!</a></h2>
-                <div className={styles.horizontal} style={{ width: "100%", display: "flex", alignItems: "center", justifyContent: "center", marginBottom: 20 }}>
-                    <div style={{ width: "30%", marginRight: 20 }}>
-                        <video className={styles.teamImage} src="/nextjs-babysteps_website/images/milestonesfunctionality.mov" alt="tutorial video" />
-                    </div>
-                    <div style={{ width: "60%" }}>
-                        <h3 style={{ fontSize: 32, color: "#0d4b5f", marginBottom: 10 }}>Isabella Bertagnolli</h3>
-                        <p style={{ fontSize: 24, color: "#000000", marginBottom: 20 }}>
-                           Check out our tutorial video for an overview of the BabySteps app and the features we
-                        </p>
+            <div style={{ display: "flex", marginTop: "64px", flexDirection: "column", width: "100%" }}>
+                <div style={{ display: "flex", alignItems: "center", justifyContent: "center", height: "150px" }}><h1 style={{ letterSpacing: "8px", fontSize: "48px" }}>Welcome to BabySteps</h1></div>
+                 <div style={{ width: "100%", background: theme.colors.secondary, display: "flex", justifyContent: "center" }}>
+                    <div style={{ display: "flex", padding: "12px 32px", gap: "12px", width: "clamp(800px, 100%, 1200px)", alignItems: "center", minHeight: "400px", flexDirection: "row" }}>
+                        <div>
+                            <img src={"/nextjs-babysteps_website/images/tutorial.jpeg"} width={300} height={300} style={{ borderRadius: "10px", objectFit: "cover" }} />
+                        </div>
+                        <div style={{ flexGrow: 1, display: "flex", flexDirection: "column", alignItems: "center", padding: "32px" }}>
+                            <h1 style={{ textTransform: "uppercase", letterSpacing: "8px" }}>Tutorial</h1>
+                            <div style={{ fontSize: "18px" }}>{
+                                <p>Check out this video to see a walkthrough of our application.</p>
+                               
+                            }</div>
+                             <Button 
+                                onClick={() => router.push("/")} 
+                                variant="text"
+                                sx={{
+                                  color: "black",
+                                  borderColor: "black",
+                                  bgcolor: "#FAF9F8",
+                                  mb:1,
+                                  ":hover": {
+                                    bgcolor: theme.colors.primary,
+                                  }
+                                }}
+                                >Download</Button>
+                        </div>
                     </div>
                 </div>
-                <hr style={{ width: "80%", color: "#E8E5DF", backgroundColor: "#E8E5DF", height: 1, border: "none" }} />
             </div>
         </>
     );
--- styles/theme.js
+++ styles/theme.js
@@ -1,7 +1,7 @@
 
 export default Object.freeze({
     colors: {
-        primary: "#FAF9F8",
+        primary: "#b3beb6",
         secondary: "#E8E5DF", //"#B3BEB6",
         tertiary: "#0d4b5f",
         text: "#ffffff"
