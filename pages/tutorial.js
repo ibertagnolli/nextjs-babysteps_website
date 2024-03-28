d7399e5ea2988787a4c53f5f05936161f272fe1f
ibertagnolli <ibertagnolli@hotmail.com>
2024-03-28 17:53:30 -0600

    adding youtube walkthrough

--- pages/tutorial.js
+++ pages/tutorial.js
@@ -18,30 +18,33 @@ const TeamMemberPage = () => {
             />
             <div style={{ display: "flex", marginTop: "64px", flexDirection: "column", width: "100%" }}>
                 <div style={{ display: "flex", alignItems: "center", justifyContent: "center", height: "150px" }}><h1 style={{ letterSpacing: "8px", fontSize: "48px" }}>Try BabySteps</h1></div>
-                 <div style={{ width: "100%", background: theme.colors.secondary, display: "flex", justifyContent: "center" }}>
-                    <div style={{ display: "flex", padding: "12px 32px", gap: "12px", width: "clamp(800px, 100%, 1200px)", alignItems: "center", minHeight: "400px", flexDirection: "row" }}>
-                        <div>
-                            <img src={"/nextjs-babysteps_website/images/tutorial.jpeg"} width={300} height={300} style={{ borderRadius: "10px", objectFit: "cover" }} />
+                <div style={{ width: "100%", background: theme.colors.secondary, display: "flex", justifyContent: "center" }}>
+                    <div style={{ display: "flex", padding: "32px 32px", gap: "12px", width: "clamp(800px, 100%, 1200px)", alignItems: "center", minHeight: "400px", flexDirection: "row" }}>
+                        <div style={{ display: "flex", padding: " 46px"}}>
+                            <iframe width="300" height="600"
+                                src="https://www.youtube.com/embed/7i4mzdLNA3M?si=6FdILv_ggdeNvN6J">
+                            </iframe>
+                            {/* <iframe width="560" height="315" src="https://www.youtube.com/embed/7i4mzdLNA3M?si=6FdILv_ggdeNvN6J" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe> */}
                         </div>
                         <div style={{ flexGrow: 1, display: "flex", flexDirection: "column", alignItems: "center", padding: "32px" }}>
                             <h1 style={{ textTransform: "uppercase", letterSpacing: "8px" }}>Tutorial</h1>
                             <div style={{ fontSize: "18px" }}>{
                                 <p>Check out this video to see a walkthrough of our application.</p>
-                               
+
                             }</div>
-                             <Button 
-                                onClick={() => router.push("/")} 
+                            <Button
+                                onClick={() => router.push("/")}
                                 variant="text"
                                 sx={{
-                                  color: "black",
-                                  borderColor: "black",
-                                  bgcolor: "#FAF9F8",
-                                  mb:1,
-                                  ":hover": {
-                                    bgcolor: theme.colors.primary,
-                                  }
+                                    color: "black",
+                                    borderColor: "black",
+                                    bgcolor: "#FAF9F8",
+                                    mb: 1,
+                                    ":hover": {
+                                        bgcolor: theme.colors.primary,
+                                    }
                                 }}
-                                >Download</Button>
+                            >Download</Button>
                         </div>
                     </div>
                 </div>
