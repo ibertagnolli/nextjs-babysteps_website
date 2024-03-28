124f0a807bfeb42e67383a4a03ff92c439182ed7
ibertagnolli <ibertagnolli@hotmail.com>
2024-03-28 11:49:26 -0600

    again images issue

--- components/MainPageHeader.js
+++ components/MainPageHeader.js
@@ -8,7 +8,7 @@ const MainPageHeader = () => {
      
        <div style={{flexDirection:"column"}}>
       <img style={{width: "200px",padding:"20px"}} 
-            src={basePath + "images/tracking.png" }
+            src={basePath + "/images/tracking.png" }
             alt = "tracking page of the Babysteps app" />
     </div>
   
