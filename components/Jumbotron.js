baf53de55d83d0600f76835c0b0fea7a78f72d22
ibertagnolli <ibertagnolli@hotmail.com>
2024-03-28 16:40:27 +0000

    Deploying to gh-pages from @ ibertagnolli/nextjs-babysteps_website@ebd6059bc3196f0512823b2aaaba522c8fbfaf4b 🚀

--- components/Jumbotron.js
+++
@@ -1,9 +0,0 @@
-export default function Jumbotron(){
-    return (
-        <img 
-            style={{width: "100vw", position: "absolute", filter: "brightness(0.5)"}} 
-            src="/images/Capstone.jpeg" 
-            alt = "capstone team photo" 
-        />
-    )
-}
--- styles/theme.js
+++
@@ -1,9 +0,0 @@
-
-export default Object.freeze({
-    colors: {
-        primary: "#FAF9F8",
-        secondary: "#E8E5DF", //"#B3BEB6",
-        tertiary: "#0d4b5f",
-        text: "#ffffff"
-    }
-});
