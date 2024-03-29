571efd10ca22f40a5cec82177da8979801aa74d0
ibertagnolli <ibertagnolli@hotmail.com>
2024-03-28 19:57:45 -0600

    changing to black and white

--- pages/team.js
+++ pages/team.js
@@ -17,7 +17,7 @@ const TeamMemberPage = () => {
             <div style={{display: "flex", marginTop: "64px", flexDirection: "column", width: "100%"}}>
                 <div style={{display: "flex", alignItems: "center", justifyContent: "center", height: "150px"}}><h1 style={{letterSpacing: "8px", fontSize: "48px"}}>Meet the Team</h1></div>
                 <TeamMemberHighlight
-                    imgSrc="/nextjs-babysteps_website/images/resized_bella.jpeg"
+                    imgSrc="/nextjs-babysteps_website/images/selfPortrait.jpg"
                     name="Isabella Bertagnolli"
                 >
                     Hi! I am a Senior studying computer science at the University of Utah. I am interested in Machine Learning, Web Development, Mobile Development, and Art. When I'm not coding, you can find me painting, hiking, biking, and climbing in the Salt Lake valley. I am currently a teaching assistant for the Mobile App programming class and have loved working on BabySteps for the past 6 months. You can reach me at <a href="irosebertagnolli@gmail.com">irosebertagnolli@gmail.com</a> or learn more at <a href="ibertagnolli.github.io">my website</a>.
