ce4f20285470a5d1167029ecbd59db4ec2d177f3
ibertagnolli <ibertagnolli@hotmail.com>
2024-04-16 08:50:49 -0600

    added audrey to team page

--- pages/team.js
+++ pages/team.js
@@ -20,14 +20,16 @@ const TeamMemberPage = () => {
                     imgSrc="/nextjs-babysteps_website/images/selfPortrait.jpg"
                     name="Isabella Bertagnolli"
                 >
-                    Hi! I am a Senior studying computer science at the University of Utah. I am interested in Machine Learning, Web Development, Mobile Development, and Art. When I'm not coding, you can find me painting, hiking, biking, and climbing in the Salt Lake valley. I am currently a teaching assistant for the Mobile App programming class and have loved working on BabySteps for the past 6 months. You can reach me at <a href="irosebertagnolli@gmail.com">irosebertagnolli@gmail.com</a> or learn more at <a href="ibertagnolli.github.io">my website</a>.
+                    Hi! I am a Senior studying computer science at the University of Utah. I am interested in Machine Learning, Web Development, Mobile Development, and Art. When I'm not coding, you can find me painting, hiking, biking, and climbing in the Salt Lake valley. I am currently a teaching assistant for the Mobile App programming class and have loved working on BabySteps for the past 6 months. You can reach me at irosebertagnolli@gmail.com or learn more at <a href="ibertagnolli.github.io">my website</a>.
                 </TeamMemberHighlight>
                 <TeamMemberHighlight
                     imgSrc="/nextjs-babysteps_website/images/ta_picture.jpg"
-                    name="Audrey Olsen"
+                    name="Audrey Olson"
                     isAltStyle
                 >
-                   BIO HERE
+                   
+I am a graduating senior from the University of Utah. I have interests in Full Stack Web Development, Mobile Development and Programming Languages. Between working on homework and beginning side projects, you can find me cuddling up with my newborn daughter and puppy. I currently work as a Software Developer at OC Tanner and have found a new passion in creating apps from working on BabySteps the past 6 months. You can reach me at audreybrynolson@gmail.com or here on <a href= "https://www.linkedin.com/in/audrey-olson-693a1a242/" >LinkedIn.</a> 
+
                 </TeamMemberHighlight>
             </div>
            
