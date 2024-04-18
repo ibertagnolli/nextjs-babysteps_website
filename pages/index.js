f03e7525266171de3b0987f727b586aad654ff44
ibertagnolli <ibertagnolli@hotmail.com>
2024-04-17 20:30:11 -0600

    fixing intro paragraph

--- pages/index.js
+++ pages/index.js
@@ -21,7 +21,7 @@ export default function MainPage() {
         {/* Content within the darker background */}
         <MainPageHeader /> 
            </div>
-
+            
            <div style={{ display: "flex", flexDirection: "column", width: "100vw", alignItems:"center" ,padding:"32px"}}>
             <div style={{ display: "flex", flexDirection: "row", width: "100vw", alignItems:"center"}}>
             <iframe width="560" height="315" src="https://www.youtube.com/embed/7EPHTb2CE7U?si=GsRREf6gpf_EStD0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen="True"></iframe>
